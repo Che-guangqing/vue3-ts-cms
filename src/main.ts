dbe3bf68bd9fad1c5429a8cec0eb9634eafc540c
车光青 <g.che@herin.ai>
2022-03-24 16:25:19 +0800

    集成router

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
-createApp(App).mount('#app')
+import router from './router'
+
+createApp(App).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   outputDir: "./build",
+  lintOnSave: false,
   chainWebpack: (config) => {
     config.resolve.alias
       .set("@", path.resolve(__dirname, "src"))
